547c17a4796475f82eddf2ad36cf12e6cb27398f
Adrián Antón <adrtler@gmail.com>
2021-12-26 19:48:41 +0100

    BackgroundSync - Posteos sin conexión

--- src/sw-template.js
+++ src/sw-template.js
@@ -3,21 +3,22 @@
 importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js'
 );
+workbox.loadModule('workbox-background-sync');
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
   new RegExp('http://localhost:4000/api/auth/renew'),
-  new NetworkFirst(),
-  'GET'
+  new NetworkFirst()
 );
 
 registerRoute(
   new RegExp('http://localhost:4000/api/events'),
-  new NetworkFirst(),
-  'GET'
+  new NetworkFirst()
 );
 
 registerRoute(
@@ -32,4 +33,16 @@ registerRoute(
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
   ),
   new CacheFirst()
+);
+
+// POST offline
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
 );
